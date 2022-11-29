c5364ce2c0e36630b0d11771d5cc1d245ce6fc53
Carl Christensen <carl.christensen98@gmail.com>
2022-11-29 17:12:18 +0100

    Updated chair scene

--- Assets/Scripts/ChairMiniGame/TrolleyMove.cs
+++ Assets/Scripts/ChairMiniGame/TrolleyMove.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
+using static InputManager;
 
 public class TrolleyMove : MonoBehaviour
 {
+    public bool canMove;
+
+    private Rigidbody rigidBody;
+    private float speed = 7.5f;
+    private Vector3 movement, clampedVelocity;
 
     void Start()
     {
-        
+        canMove = true;
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (canMove)
+        {
+            movement = new Vector3(instance.horizontal.ReadValue<float>(), 0, 0);
+            rigidBody.AddForce(movement * speed, ForceMode.Acceleration);
+        }
     }
 }
