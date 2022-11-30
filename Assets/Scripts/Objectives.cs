266591a636ad519ed6f55a25f7b47b7e3e229adf
Sappels <58121086+Sappels@users.noreply.github.com>
2022-11-30 11:34:57 +0100

    Objectives functionality

---
+++ Assets/Scripts/Objectives.cs
@@ -0,0 +1,51 @@
+using DG.Tweening.Core.Easing;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using static PlayerController;
+
+public class Objectives : MonoBehaviour
+{
+    public int talkToObjective;
+    public int miniGamesObjective;
+    public int objectiveProgress = 0;
+    public int miniGamesPlayed = 0;
+
+    public List<string> npcNames = new List<string>();
+    [SerializeField] Transform npcParent;
+    [SerializeField] Transform miniGamesParent;
+
+    private static Objectives instance;
+    public static Objectives Instance { get { return instance; } }
+
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        talkToObjective = npcParent.childCount;
+        //miniGamesObjective = miniGamesParent.childCount;
+    }
+
+    public void CheckObjective(string name)
+    {
+        if (npcNames.Contains(name)) return;
+    
+        Debug.Log("kallad, action");
+        npcNames.Add(name);
+        objectiveProgress++;
+        Debug.Log(objectiveProgress + "/" + talkToObjective);
+        return;
+
+    }
+
+    public void AddMiniGamesPlayedProgress()
+    {
+        miniGamesPlayed++;
+        //Debug.Log(miniGamesPlayed + "/" + miniGamesObjective);
+
+    }
+}
