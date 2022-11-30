d51020de0b2199d04a2a6ba1561b4f1993699549
NeoLjusica <jessiicaceder@hotmail.com>
2022-11-30 14:00:32 +0100

    New tags

---
+++ Assets/Scripts/Player/RobotCollissions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RobotCollissions : MonoBehaviour
+{
+    public bool isInPlayArea;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        
+    }
+}
